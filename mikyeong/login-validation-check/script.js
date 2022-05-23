879996349b7cd394d5437b666218483572c9b9a9
hongmikyeong <hongmikyeong11@gmail.com>
2022-05-23 20:47:36 +0900

    이전 파일은 잘못된 파일임

--- mikyeong/login-validation-check/script.js
+++ mikyeong/login-validation-check/script.js
@@ -1,10 +1,95 @@
-// 동영상 강의에 나온 코드를 그대로 실습하세요
-// TODO : DOM으로부터 필요한 엘리먼트를 불러오세요.
+const userName = document.querySelector("#userName");
+const userPassword = document.querySelector("#password");
+const userRePassword = document.querySelector("#password-retype");
+const submit = document.querySelector("#submit");
+const input = document.querySelector("input");
+const isCorrect = {
+  userName: false,
+  userPassword: false,
+  userRePassword: false,
+};
 
-function isMoreThan4Length(value) {
-  // TODO : 동영상 강의를 보고 이 함수를 완성하세요.
-}
+// 정규표현식 : 아이디
+const regExpId = (value) => {
+  return /^[a-z0-9][a-z0-9]{5,20}$/.test(value);
+};
 
-function isMatch (password1, password2) {
-  // TODO : 동영상 강의를 보고 이 함수를 완성하세요.
-}
+// 정규표현식 : 비밀번호
+const regExpPw = (value) => {
+  return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/.test(
+    value
+  );
+};
+
+// 아이디 패턴에 따라 메세지 표시
+const isCorrectId = () => {
+  const successMsg = document.querySelector(".id-success-msg");
+  const failureMsg = document.querySelector(".id-failure-msg");
+
+  if (regExpId(userName.value)) {
+    successMsg.classList.remove("hide");
+    failureMsg.classList.add("hide");
+    isCorrect.userName = true;
+  } else {
+    successMsg.classList.add("hide");
+    failureMsg.classList.remove("hide");
+    isCorrect.userName = false;
+  }
+};
+
+// 비밀번호 패턴에 따라 메세지 표시
+const isCorrectPw = () => {
+  const mismatchMsg = document.querySelector(".pw-mismatch-msg");
+  if (regExpPw(userPassword.value)) {
+    mismatchMsg.classList.add("hide");
+    isCorrect.userPassword = true;
+  } else {
+    mismatchMsg.classList.remove("hide");
+    isCorrect.userPassword = false;
+  }
+};
+
+// 비밀번호 재확인 시 메세지 표시
+const isRecorrectPw = () => {
+  const mismatchMsg = document.querySelector(".rePw-mismatch-msg");
+  if (userPassword.value === userRePassword.value) {
+    mismatchMsg.classList.add("hide");
+    isCorrect.userRePassword = true;
+  } else {
+    mismatchMsg.classList.remove("hide");
+    isCorrect.userRePassword = false;
+  }
+};
+
+// 회원가입 조건 확인
+const joinCondition = (obj) => {
+  for (const value of Object.values(obj)) {
+    if (!value) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// 회원가입 버튼 클릭
+const join = () => {
+  const modal = document.querySelector(".modal__container");
+  const modalContent = modal.querySelector(".modal__content");
+  let msg = `${userName.value}님,\r\n 스터디 semantic의\r\n 회원가입을 축하드립니다!`;
+
+  if (joinCondition(isCorrect)) {
+    modal.classList.remove("hide");
+    modalContent.textContent = msg;
+  } else {
+    modal.classList.add("hide");
+    const failureMsg = document.querySelectorAll(".failure");
+    for (const msg of failureMsg) {
+      msg.classList.remove("hide");
+    }
+  }
+};
+
+userName.addEventListener("keyup", isCorrectId);
+userPassword.addEventListener("keyup", isCorrectPw);
+userRePassword.addEventListener("keyup", isRecorrectPw);
+submit.addEventListener("click", join);
