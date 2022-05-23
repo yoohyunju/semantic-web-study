01e45361ae6095795650b05f10ec49eb4babd9a4
hongmikyeong <hongmikyeong11@gmail.com>
2022-05-23 10:33:06 +0900

    회원가입 기능 완료

---
+++ mikyeong/login-validation-check/script.js
@@ -0,0 +1,10 @@
+// 동영상 강의에 나온 코드를 그대로 실습하세요
+// TODO : DOM으로부터 필요한 엘리먼트를 불러오세요.
+
+function isMoreThan4Length(value) {
+  // TODO : 동영상 강의를 보고 이 함수를 완성하세요.
+}
+
+function isMatch (password1, password2) {
+  // TODO : 동영상 강의를 보고 이 함수를 완성하세요.
+}
